9615389416020b2238598f246b0171a399f57b03
Manjunath Somashekar <ujnamss@gmail.com>
2019-08-15 18:28:02 -0700

    refactored code

--- project_scaffold.py
+++ project_scaffold.py
@@ -3,36 +3,45 @@ import sys
 from os import path
 from pathlib import Path
 
-def create_project_sub_dir(project_root, name, dir_name, home_dir, port, files = []):
-    project_sub_dir = "{}/{}".format(project_root, dir_name)
-    Path(project_sub_dir).mkdir(parents=True, exist_ok=True)
-    for file in files:
-        template_path = "templates/{}".format(file)
-        file_path = "{}/{}".format(project_sub_dir, file)
-        if Path(template_path).exists():
-            with open(template_path, "r", encoding="utf-8") as f_in, open(file_path, "w", encoding="utf-8") as f_out:
-                contents = f_in.read()
-                contents = contents.replace("{{HOME_DIR}}", home_dir)
-                contents = contents.replace("{{PORT}}", port)
-                contents = contents.replace("{{service_name}}", name)
-                f_out.write(contents)
-        else:
-            Path(file_path).touch()
+class ScaffoldBuilder:
 
-def create_project(dir_path, name, home_dir, port):
-    project_root = "{}/{}".format(dir_path, name)
-    Path(project_root).mkdir(parents=True, exist_ok=True)
+    def __init__(self, project_root, name, home_dir, port, network_name):
+        self.project_root = project_root
+        self.name = name
+        self.network_name = network_name
+        self.home_dir = home_dir
+        self.port = port
 
-    create_project_sub_dir(project_root, name, 'config', home_dir, port, files = ['test.cfg'])
-    create_project_sub_dir(project_root, name, 'install', home_dir, port, files = ['requirements.txt'])
-    create_project_sub_dir(project_root, name, 'src', home_dir, port, files = ['app.py'])
-    create_project_sub_dir(project_root, name, 'tests', home_dir, port, files = ['basic_tests.py'])
-    create_project_sub_dir(project_root, name, ".", home_dir, port, files = ['.dockerignore', '.env', 'docker-compose.yml', 'Dockerfile', 'run-docker.sh'])
+    def create_project_sub_dir(self, dir_name, files = []):
+        root_dir = "{}/{}".format(self.project_root, self.name)
+        project_sub_dir = "{}/{}".format(root_dir, dir_name)
+        Path(project_sub_dir).mkdir(parents=True, exist_ok=True)
+        for file in files:
+            template_path = "templates/{}".format(file)
+            file_path = "{}/{}".format(project_sub_dir, file)
+            if Path(template_path).exists():
+                with open(template_path, "r", encoding="utf-8") as f_in, open(file_path, "w", encoding="utf-8") as f_out:
+                    contents = f_in.read()
+                    contents = contents.replace("{{HOME_DIR}}", self.home_dir)
+                    contents = contents.replace("{{PORT}}", self.port)
+                    contents = contents.replace("{{service_name}}", self.name)
+                    contents = contents.replace("{{network_name}}", self.network_name)
+                    f_out.write(contents)
+            else:
+                Path(file_path).touch()
+
+    def create_project(self):
+        self.create_project_sub_dir('config', files = ['test.cfg'])
+        self.create_project_sub_dir('install', files = ['requirements.txt'])
+        self.create_project_sub_dir('src', files = ['app.py'])
+        self.create_project_sub_dir('tests', files = ['basic_tests.py'])
+        self.create_project_sub_dir(".", files = ['.dockerignore', '.env', '.gitignore', 'docker-compose.yml', 'Dockerfile', 'run-docker.sh'])
 
 if __name__ == '__main__':
     no_of_args = len(sys.argv)
-    if no_of_args != 5:
-        print("Usage: python project_scaffold.py <dir> <name> <home_dir> <port>")
+    if no_of_args != 6:
+        print("Usage: python project_scaffold.py <dir> <name> <home_dir> <port> <network_name>")
         exit(1)
 
-    create_project(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    sb = ScaffoldBuilder(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    sb.create_project()
